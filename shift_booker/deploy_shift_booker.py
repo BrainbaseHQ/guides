2f9ffafa91939590476df0e065a80710ed346a10
Abhinav Tumu <abhinav.tumu1@gmail.com>
2025-03-02 23:22:06 -0800

    shift booker trace edits

--- shift_booker/deploy_shift_booker.py
+++ shift_booker/deploy_shift_booker.py
@@ -9,17 +9,17 @@ import os
 
 bb = BrainbaseLabs(api_key=os.getenv("BB_API_KEY"))
 
-twilio_integration = bb.team.integrations.twilio.create(
-    account_sid=os.getenv("TWILIO_ACCOUNT_SID"),
-    auth_token=os.getenv("TWILIO_AUTH_TOKEN")
-)
+# twilio_integration = bb.team.integrations.twilio.create(
+#     account_sid=os.getenv("TWILIO_ACCOUNT_SID"),
+#     auth_token=os.getenv("TWILIO_AUTH_TOKEN")
+# )
 
 raw_phone_number = "+18333916030"
 
-phone_number = bb.team.assets.register_phone_number(
-    phone_number=raw_phone_number,
-    integration_id=twilio_integration.id
-)
+# phone_number = bb.team.assets.register_phone_number(
+#     phone_number=raw_phone_number,
+#     integration_id=twilio_integration.id
+# )
 
 worker = bb.workers.create(name="My Shift Booker", description="A shift booking assistant", status="active")
 
