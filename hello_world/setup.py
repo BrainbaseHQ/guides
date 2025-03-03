53c3b4129dc38d08be4d506093b55ade140b7d04
Abhinav Tumu <abhinav.tumu1@gmail.com>
2025-03-02 23:24:13 -0800

    create worker & flow

---
+++ hello_world/setup.py
@@ -0,0 +1,34 @@
+from brainbase_labs import BrainbaseLabs
+import os
+
+# load from .env
+from dotenv import load_dotenv
+load_dotenv()
+
+# create brainbase client
+bb = BrainbaseLabs(
+    base_url=os.environ["BRAINBASE_BASE_URL"],
+    api_key=os.environ["BRAINBASE_API_KEY"]
+)
+
+# 1. list workers
+workers = bb.workers.list()
+# print(workers)
+
+# 2. create worker
+new_worker = bb.workers.create(
+    name="Hello World Worker",
+    description="Converse with the user",
+    status="active"
+)
+print(new_worker.id)
+
+# 3. create new flow version
+new_flow = bb.workers.flows.create(
+    worker_id=new_worker.id,
+    path="1_hello_world.based",
+    name="Hello World",
+    label="v1",
+    validate=False
+)
+print(new_flow.id)
