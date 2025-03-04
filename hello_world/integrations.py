746f27bf5a285aa17a6cb349c55edbbe85cba88f
Abhinav Tumu <abhinav.tumu1@gmail.com>
2025-03-04 10:31:28 -0800

    integrations guide

---
+++ hello_world/integrations.py
@@ -0,0 +1,22 @@
+from brainbase_labs import BrainbaseLabs
+import os
+
+# load from .env
+from dotenv import load_dotenv
+load_dotenv()
+
+# create brainbase client
+api_key = os.getenv("BRAINBASE_API_KEY")
+
+print(api_key)
+bb = BrainbaseLabs(api_key=os.getenv("BRAINBASE_API_KEY"))
+
+# 1. list integrations
+integrations = bb.team.integrations.list()
+print(integrations)
+
+
+# 2. list phone numbers
+phone_numbers = bb.team.assets.list_phone_numbers()
+print(phone_numbers)
+
